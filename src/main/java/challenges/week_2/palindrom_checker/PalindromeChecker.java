42927908db546a5072abb8606df428baed089eda
Alexander Jarrett <alexjarrett400@gmail.com>
2021-12-06 13:34:39 +0000

    Adding comments to the palindrome checker class

--- src/main/java/challenges/week_2/palindrom_checker/PalindromeChecker.java
+++ src/main/java/challenges/week_2/palindrom_checker/PalindromeChecker.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 
 public class PalindromeChecker {
 
+    /*
+    This method intakes a string and returns a boolean for if it is a palindrome or not.
+     */
     public boolean isPalindrome(String wordToCheck){
         char[] charsToCheck = splitWord(wordToCheck);
         for (int i = 0; i < charsToCheck.length; i++) {
@@ -16,18 +19,16 @@ public class PalindromeChecker {
     }
 
 
+    //This method splits a string into a char array and makes sure its all the same case to be checked
     public char[] splitWord(String wordToSplit){
         return wordToSplit.toLowerCase().toCharArray();
-    }
-
-    public String[] splitSentence(String sentenceToSplit){
-        return sentenceToSplit.split(" ");
     }
 
     public PalindromeChecker() {
 
     }
 
+    //This method is used to split a sentence to then check which words are palindromes and returns a boolean array
     public boolean[] sentenceChecker(String sentenceToCheck){
         String[] splitSentence = sentenceToCheck.split(" ");
         boolean[] whichWordsPali = new boolean[splitSentence.length];
