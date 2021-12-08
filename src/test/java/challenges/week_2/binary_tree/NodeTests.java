8f808619209cb4dc50203723a2021d94d6813f6a
Alexander Jarrett <alexjarrett400@gmail.com>
2021-12-08 09:09:27 +0000

    Adding Tests for BinaryTree and Node

--- src/test/java/challenges/week_2/binary_tree/NodeTests.java
+++ src/test/java/challenges/week_2/binary_tree/NodeTests.java
@@ -1,4 +1,21 @@
 package challenges.week_2.binary_tree;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class NodeTests {
+
+    Node node = new Node(8);
+    Node leftTestNode = new Node(6);
+
+
+    @Test
+    public void nodeTests(){
+        node.setLeftChild(leftTestNode);
+        Assertions.assertEquals(node.getValue(), 8);
+        Assertions.assertEquals(node.isLeftChildEmpty(), false);
+        Assertions.assertEquals(node.isRightChildEmpty(), true);
+        Assertions.assertEquals(node.getLeftChild(), leftTestNode);
+
+    }
 }
